c0a2e91c9228f2afd9946e92d5dab0d1454262b5
Akshay KM <akshay194@gmail.com>
2022-01-26 19:40:32 +0530

    responsive bug fixed

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 function Projects() {
   return <div className=' h-screen pl-9'>
-      <h1 className='font-medium text-xl sm:text-4xl'>SOME OF MY WORKS AND PERSONAL PROJECTS</h1>
-      <div className=' p-5 sm:flex justify-center items-center pt-24'>
+      <h1 className=' pt-9 font-medium text-xl sm:text-4xl'>SOME OF MY WORKS AND PERSONAL PROJECTS</h1>
+      <div className='pr-4 sm:flex justify-center items-center pt-24'>
         <a href="https://akshaylkm.github.io/netflix/"><img className=' w-96 h-34 mr-9 mb-5 hover:scale-125 shadow-xl' src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt="img" /></a>
         <a href="https://akshaylkm.github.io/wtc-social-media-app/"><img className=' w-96 h-34 mr-9 mb-5 hover:scale-125 shadow-xl' src="https://i.ytimg.com/vi/NljIHlZRTTE/maxresdefault.jpg" alt="img" /></a>
         <a href="https://akshaylkm.github.io/counter-responsive/"><img className=' w-96 h-34 mr-9 mb-5 hover:scale-125 shadow-xl' src="https://store-images.s-microsoft.com/image/apps.32388.9007199266655303.9a968b1f-98f8-41ae-b822-eefdea30416f.8a1c0f7b-c155-48fc-a4cb-7b4e980dba35?w=672&h=378&q=80&mode=letterbox&background=%23FFE4E4E4&format=jpg" alt="img" /></a>
       </div>
-      <div className=' pl-4 pt-10 sm:pt-32'>
+      <div className=' pt-10 sm:pt-32'>
         <h1 className='font-medium text-2xl sm:text-4xl'>Have an idea let's build it.</h1>
         <p className='pr-1 text-xl font-extralight sm:text-2xl'>Feel free to reach out if you’re in need of a Developer.
            Whether that means you need a website,<br /> web app, or you’re looking to fill a full-time position.
