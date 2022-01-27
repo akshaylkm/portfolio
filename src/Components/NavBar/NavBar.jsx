3e7734838e015f82a1dcfa279eaa2c1e7e53d9fb
Akshay KM <akshay194@gmail.com>
2022-01-27 22:57:05 +0530

    project component aos added

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar() {
   return <div className=" h-24 p-9">
           <ul className='flex justify-between text-xl font-semibold'>
               <li className=' cursor-pointer'>AKSHAY <span className='font-light'>K</span>M</li>
-              <li className=' cursor-pointer'>PROJECTS</li>
+              <li className=' cursor-pointer font-light'>PROJECTS</li>
           </ul>
       </div>;
 }
--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -7,20 +7,20 @@ function Skills() {
       <p className='font-extralight text-md pt-7 sm:text-2xl'>From the beginning days of when I first got into software development up until now, I’ve learned and gained a solid understanding of web development standards, the importance of reusability/scalability in large enterprise applications, and how user expereince means everything.</p>
       </div>
       <div data-aos="fade-left" className='basis-1/2 pl-9 pt-1 pb-7 sm:pt-20'>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>Javascript ES6</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>REACT JS</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>GO</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>FRONT END DEVELOPMENT</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>GIT</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>HTML5</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>CSS</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>TAILWIND CSS</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>PRODUCT DESIGN</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>RESEARCH AND DEVELOPMENT</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>PRODUCT MANAGEMENT</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>FIGMA</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>UI DESIGN AND DEVELOPMENT</button>
-          <button className=' rounded-sm bg-slate-100 mr-5 mb-3'>PRESENTATION SKILL</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>Javascript ES6</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>REACT JS</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>GO</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>FRONT END DEVELOPMENT</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>GIT</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>HTML5</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>CSS</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>TAILWIND CSS</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>PRODUCT DESIGN</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>RESEARCH AND DEVELOPMENT</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>PRODUCT MANAGEMENT</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>FIGMA</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>UI DESIGN AND DEVELOPMENT</button>
+          <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>PRESENTATION SKILL</button>
       </div>
   </div>;
 }
