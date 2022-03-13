814cd354630cafea2c6a45975775cb554a13c923
Akshay KM <akshay194@gmail.com>
2022-03-13 23:49:08 +0530

    skill component image added

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import img1 from '../../Images/www.jpg';
 function Skills() {
-  return <div className='h-screen sm:flex'>
+  return <div className=' h-fit sm:flex'>
       <div data-aos="fade-right" className=' basis-1/2 p-5 sm:pl-9'>
       <h1 className='font-bold text-3xl sm:text-4xl'>MY SKILLS</h1>
       <p className='font-extralight text-md pt-7 sm:text-2xl'>From the beginning days of when I first got into software development up until now, I’ve learned and gained a solid understanding of web development standards, the importance of reusability/scalability in large enterprise applications, and how user expereince means everything.</p>
       </div>
+      <img data-aos="fade-right" className=' w-72 h-96 rounded-2xl shadow-2xl ml-5' src={img1} alt="ímg" />
       <div data-aos="fade-right" className='basis-1/2 pl-9 pt-1 pb-7 sm:pt-20'>
           <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>Javascript ES6</button>
           <button className=' rounded-sm bg-slate-100 mr-5 mb-3 px-2'>REACT JS</button>
