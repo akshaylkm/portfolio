57f4451b1764090079f8cd47ac000590d990fe5a
Akshay KM <akshay194@gmail.com>
2022-04-25 11:47:04 +0530

    helloApp project added

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import expenseImg from '../../Images/expense.png';
+import HelloApp from '../../Images/h.JPG';
+
 function Projects() {
   return <div data-aos="fade-up" className=' h-auto pl-9'>
       <h1 className=' pt-9 font-medium text-xl sm:text-4xl'>SOME OF MY WORKS AND PERSONAL PROJECTS</h1>
@@ -7,6 +9,7 @@ function Projects() {
         <a href="https://akshaylkm.github.io/netflix/"><img className=' w-96 h-34 mr-9 mb-5 sm:hover:scale-125 shadow-xl' src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt="img" /></a>
         <a href="https://akshaylkm.github.io/Expense/"><img className=' w-96 h-34 mr-9 mb-5 sm:hover:scale-125 shadow-xl' src={expenseImg}alt="img" /></a>
         <a href="https://akshaylkm.github.io/wtc-social-media-app/"><img className=' w-96 h-34 mr-9 mb-5 sm:hover:scale-125 shadow-xl' src="https://i.ytimg.com/vi/NljIHlZRTTE/maxresdefault.jpg" alt="img" /></a>
+        <a href="https://akshaylkm.github.io/HelloApp/"><img className=' w-96 h-34 mr-9 mb-5 sm:hover:scale-125 shadow-xl' src={HelloApp}alt="img" /></a>
         <a href="https://akshaylkm.github.io/counter-responsive/"><img className=' w-96 h-34 mr-9 mb-5 sm:hover:scale-125 shadow-xl' src="https://store-images.s-microsoft.com/image/apps.32388.9007199266655303.9a968b1f-98f8-41ae-b822-eefdea30416f.8a1c0f7b-c155-48fc-a4cb-7b4e980dba35?w=672&h=378&q=80&mode=letterbox&background=%23FFE4E4E4&format=jpg" alt="img" /></a>
       </div>
       <div className=' pt-10 sm:pt-32 h-96'>
